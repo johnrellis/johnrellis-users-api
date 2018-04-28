7f202df0b6405edc0e903a0528ab4d6a8885151f
John Rellis <john.d.rellis@gmail.com>
2018-04-28 11:15:54 +0100

    added mongoose with intial basic connection and 'I am alive' proof

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const log = require('winston');
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', function(error) {
+    log.error(error);
+});
+db.once('open', function() {
+    log.info('connected to mongo');
+});
+
+app.get('/', (req, res) => res.send('I am alive'));
+
+
+module.exports = app.listen(3000, () => log.info('users api listening on port 3000!'));
