e6fac4b50e34c3a429f41429923e44d0f4304fb5
John Rellis <john.d.rellis@gmail.com>
2018-04-29 10:23:31 +0100

    added tests for transformIdOutgoing

--- routes/v1/transformIdOutgoing.js
+++ routes/v1/transformIdOutgoing.js
@@ -1,10 +1,18 @@
 'use strict';
 
+const _ = require('lodash');
+
 /**
 * If outgoing object contains _id, it is replaced with a property called id with the same value.  _id is removed
-* @param  {Object} outgoingObject object to be transformed
-* @return {Object}                a copy of the object with the 
+* @param  {Object} objectToBeTransformed object to be transformed
+* @return {Object}                a copy of the object with _id converted to id
 */
-module.exports = () => {
-
+module.exports = (objectToBeTransformed) => {
+    let clonedOutgoing = {};
+    if(objectToBeTransformed){
+        clonedOutgoing = _.cloneDeep(objectToBeTransformed);
+        clonedOutgoing.id = objectToBeTransformed._id;
+        delete clonedOutgoing._id;
+    } 
+    return clonedOutgoing;
 };
--- test/unit/routes/v1/unit.test.transformIdOutgoing.js
+++ test/unit/routes/v1/unit.test.transformIdOutgoing.js
@@ -8,8 +8,9 @@ describe('transformIdOutgoing', function() {
     it('should transform _id to id in an outgoing object', function(done) {
         let outgoingObject = {_id : 123456, email:'john@home.com'};
         let transformIdOutgoing = require(`${process.cwd()}/routes/${version}/transformIdOutgoing`);
-        let transformed = transformIdOutgoing();
+        let transformed = transformIdOutgoing(outgoingObject);
         expect(transformed.id).to.exist;
+        expect(transformed).to.not.equal(outgoingObject);
         expect(transformed.id).to.equal(outgoingObject._id);
         expect(transformed._id).to.not.exist;
         expect(transformed.email).to.equal(outgoingObject.email);
@@ -21,7 +22,7 @@ describe('transformIdOutgoing', function() {
     it('should remove _id from object when it is set to undefined', function(done) {
         let outgoingObject = {_id : undefined, email:'john@home.com'};
         let transformIdOutgoing = require(`${process.cwd()}/routes/${version}/transformIdOutgoing`);
-        let transformed = transformIdOutgoing();
+        let transformed = transformIdOutgoing(outgoingObject);
         expect(transformed.id).to.not.exist;
         expect(transformed._id).to.not.exist;
         expect(transformed.email).to.equal(outgoingObject.email);
@@ -32,10 +33,20 @@ describe('transformIdOutgoing', function() {
     it('should do nothing when _id is not present', function(done) {
         let outgoingObject = {email:'john@home.com'};
         let transformIdOutgoing = require(`${process.cwd()}/routes/${version}/transformIdOutgoing`);
-        let transformed = transformIdOutgoing();
+        let transformed = transformIdOutgoing(outgoingObject);
         expect(transformed.id).to.not.exist;
         expect(transformed._id).to.not.exist;
         expect(transformed.email).to.equal(outgoingObject.email);
+        done();
+    });
+
+    it('should return an empty object when outgoingObject is not defined', function(done) {
+        let transformIdOutgoing = require(`${process.cwd()}/routes/${version}/transformIdOutgoing`);
+        let transformed = transformIdOutgoing(undefined);
+        expect(transformed).to.eql({});
+        expect(transformed.id).to.not.exist;
+        expect(transformed._id).to.not.exist;
+        expect(transformed.email).to.not.exist;
         done();
     });
 });
