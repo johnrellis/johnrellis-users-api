4eeeb7c78bbb3cdeea8e6809f356a51d2a7eb838
John Rellis <john.d.rellis@gmail.com>
2024-10-22 09:14:25 +0100

    Suggest improvements for my API
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/johnrellis/johnrellis-users-api?shareId=XXXX-XXXX-XXXX-XXXX).

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const express = require('express');
 const app = express();
 const log = require('winston');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 app.use(bodyParser.json()); // for parsing application/json
 
@@ -23,7 +25,21 @@ db.once('open', function() {
     log.info('connected to mongo');
 });
 
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        // Here you would look up the user in your DB and check the password
+        // For now, we'll just pretend the user is authenticated
+        if (username === 'user' && password === 'password') {
+            return done(null, { id: 1, username: 'user' });
+        } else {
+            return done(null, false, { message: 'Incorrect username or password.' });
+        }
+    }
+));
+
+app.use(passport.initialize());
+
 app.get('/', (req, res) => res.send('I am alive'));
-app.use('/api/v1/', require('./routes/v1/'));
+app.use('/api/v1/', passport.authenticate('local', { session: false }), require('./routes/v1/'));
 
 module.exports = app.listen(3000, () => log.info('users api listening on port 3000!'));
